033aa865c160fee8a2c94693a69161c6c583ec2d
Sergio Rodríguez García <sergiorg2011@gmail.com>
2026-04-20 09:39:37 +1000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.getElementById("hamburger");
+    const navLinks = document.getElementById("nav-links");
+    
+    if (hamburger && navLinks) {
+        hamburger.addEventListener("click", () => {
+            navLinks.classList.toggle("active");
+            hamburger.classList.toggle("active");
+        });
+    }
+});
